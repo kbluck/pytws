37d2bcc122533cd408776bba008776696fc08628
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-11-20 19:46:38 +0000

    Implemented tws.Util

---
+++ trunk/test_tws/test_Util.py
@@ -0,0 +1,106 @@
+'''Unit test package for module "tws.Util".'''
+
+__copyright__ = "Copyright (c) 2008 Kevin J Bluck"
+__version__   = "$Id$"
+
+import unittest
+from tws import Util
+
+
+class test_Util(unittest.TestCase):
+    '''Test free functions in module "tws.Util"'''
+
+    def test_StringIsEmpty(self):
+        self.assertTrue(Util.StringIsEmpty(""))
+        self.assertTrue(Util.StringIsEmpty(None))
+        self.assertFalse(Util.StringIsEmpty("0"))
+
+        if __debug__:
+            self.assertRaises(AssertionError, Util.StringIsEmpty, 0)
+
+    def test_NormalizeString(self):
+        self.assertEqual("", Util.NormalizeString(None))
+        self.assertEqual("", Util.NormalizeString(""))
+        self.assertEqual("123", Util.NormalizeString("123"))
+
+        if __debug__:
+            self.assertRaises(AssertionError, Util.NormalizeString, 0)
+
+    def test_StringCompare(self):
+        self.assertTrue(Util.StringCompare(None, None))
+        self.assertTrue(Util.StringCompare("", None))
+        self.assertTrue(Util.StringCompare(None,""))
+        self.assertTrue(Util.StringCompare("123","123"))
+        self.assertTrue(Util.StringCompare("ABC","ABC"))
+        self.assertTrue(Util.StringCompare("xyz","xyz"))
+
+        self.assertFalse(Util.StringCompare("123", None))
+        self.assertFalse(Util.StringCompare(None, "123"))
+        self.assertFalse(Util.StringCompare("123", ""))
+        self.assertFalse(Util.StringCompare("", "123"))
+        self.assertFalse(Util.StringCompare("123", "321"))
+        self.assertFalse(Util.StringCompare("321", "123"))
+        self.assertFalse(Util.StringCompare("ABC","abc"))
+        self.assertFalse(Util.StringCompare("abc","ABC"))
+
+        if __debug__:
+            self.assertRaises(AssertionError, Util.StringCompare, 0, "")
+            self.assertRaises(AssertionError, Util.StringCompare, "", 0)
+            self.assertRaises(AssertionError, Util.StringCompare, 0, 0)
+
+    def test_StringCompareIgnCase(self):
+        self.assertTrue(Util.StringCompareIgnCase(None, None))
+        self.assertTrue(Util.StringCompareIgnCase("", None))
+        self.assertTrue(Util.StringCompareIgnCase(None,""))
+        self.assertTrue(Util.StringCompareIgnCase("123","123"))
+        self.assertTrue(Util.StringCompareIgnCase("ABC","ABC"))
+        self.assertTrue(Util.StringCompareIgnCase("xyz","xyz"))
+        self.assertTrue(Util.StringCompareIgnCase("ABC","abc"))
+        self.assertTrue(Util.StringCompareIgnCase("abc","ABC"))
+
+        self.assertFalse(Util.StringCompareIgnCase("123", None))
+        self.assertFalse(Util.StringCompareIgnCase(None, "123"))
+        self.assertFalse(Util.StringCompareIgnCase("123", ""))
+        self.assertFalse(Util.StringCompareIgnCase("", "123"))
+        self.assertFalse(Util.StringCompareIgnCase("123", "321"))
+        self.assertFalse(Util.StringCompareIgnCase("321", "123"))
+
+        if __debug__:
+            self.assertRaises(AssertionError, Util.StringCompare, 0, "")
+            self.assertRaises(AssertionError, Util.StringCompare, "", 0)
+            self.assertRaises(AssertionError, Util.StringCompare, 0, 0)
+
+    def test_VectorEqualsUnordered(self):
+        self.assertTrue(Util.VectorEqualsUnordered([0,1,2], [0,1,2]))
+        self.assertTrue(Util.VectorEqualsUnordered([0,1,2], [2,1,0]))
+        self.assertTrue(Util.VectorEqualsUnordered([2,1,0], [0,1,2]))
+        self.assertTrue(Util.VectorEqualsUnordered([1,2,0], [0,1,2]))
+
+        self.assertFalse(Util.VectorEqualsUnordered([0,1,2], [0,1,1]))
+        self.assertFalse(Util.VectorEqualsUnordered([0,1,2], [0,1]))
+        self.assertFalse(Util.VectorEqualsUnordered([0,1,2], [0,1,2,3]))
+
+        if __debug__:
+            self.assertRaises(AssertionError, Util.VectorEqualsUnordered, "", "")
+            self.assertRaises(AssertionError, Util.VectorEqualsUnordered, [], "")
+            self.assertRaises(AssertionError, Util.VectorEqualsUnordered, "", [])
+
+    def test_IntMaxString(self):
+        self.assertEqual("0", Util.IntMaxString(0))
+        self.assertEqual("1", Util.IntMaxString(1))
+        self.assertEqual("-1", Util.IntMaxString(-1))
+        self.assertEqual("", Util.IntMaxString(Util._INT_MAX_VALUE))
+
+        if __debug__:
+            self.assertRaises(AssertionError, Util.IntMaxString, "0")
+            self.assertRaises(AssertionError, Util.IntMaxString, 0.0)
+
+    def test_DoubleMaxString(self):
+        self.assertEqual("0.0", Util.DoubleMaxString(0.0))
+        self.assertEqual("1.0", Util.DoubleMaxString(1.0))
+        self.assertEqual("-1.0", Util.DoubleMaxString(-1.0))
+        self.assertEqual("", Util.DoubleMaxString(Util._DOUBLE_MAX_VALUE))
+
+        if __debug__:
+            self.assertRaises(AssertionError, Util.DoubleMaxString, "0")
+            self.assertRaises(AssertionError, Util.DoubleMaxString, 0)
---
+++ trunk/tws/Util.py
@@ -0,0 +1,70 @@
+'''Utility functions.
+
+   Various utility functions implemented by the TWS API. Mostly have to do 
+   with string and list comparison.
+'''
+
+__copyright__ = "Copyright (c) 2008 Kevin J Bluck"
+__version__   = "$Id$"
+
+
+def StringIsEmpty(strval):
+    assert issubclass(type(strval), str) or (strval == None) 
+
+    return not strval
+
+
+def NormalizeString(strval):
+    assert issubclass(type(strval), str) or (strval == None)  
+
+    return strval if strval else ""
+
+
+def StringCompare(lhs, rhs):
+    assert issubclass(type(lhs), str) or (lhs == None)  
+    assert issubclass(type(rhs), str) or (rhs == None)  
+
+    return (NormalizeString(lhs) == NormalizeString(rhs))
+
+
+def StringCompareIgnCase(lhs, rhs):
+    assert issubclass(type(lhs), str) or (lhs == None)  
+    assert issubclass(type(rhs), str) or (rhs == None)  
+
+    return (NormalizeString(lhs).lower() == NormalizeString(rhs).lower())
+
+
+def VectorEqualsUnordered(lhs, rhs):
+    assert issubclass(type(lhs), list) or (lhs == None) 
+    assert issubclass(type(rhs), list) or (rhs == None)
+
+    # Convert None values to empty lists
+    if lhs is None: lhs = []
+    if rhs is None: rhs = []
+
+    # Short-circuit if the lists already match.
+    if (lhs == rhs): return True
+
+    # Make sorted copies of lists and return results of compare.
+    sorted_lhs = list(lhs)    
+    sorted_rhs = list(rhs)
+    sorted_lhs.sort()    
+    sorted_rhs.sort()    
+    return sorted_lhs == sorted_rhs    
+
+
+def IntMaxString(value):
+    assert issubclass(type(value), int) 
+
+    return str(value) if (value < _INT_MAX_VALUE) else ""
+
+
+def DoubleMaxString(value):
+    assert issubclass(type(value), float) 
+
+    return str(value) if (value < _DOUBLE_MAX_VALUE) else ""
+
+
+# Java definitions of max values for Int and Double types
+_INT_MAX_VALUE = int(2**31-1)
+_DOUBLE_MAX_VALUE = float((2-2**-52)*2**1023)
