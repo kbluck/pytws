b315c450dbe2562e1c97e36551d4755c29899bf7
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-11-20 19:17:28 +0000

    Rename 'pytws' package to just 'tws'.

---
+++ trunk/test_tws/__init__.py
@@ -0,0 +1,10 @@
+'''Unit test package for package "tws".'''
+
+__copyright__ = "Copyright (c) 2008 Kevin J Bluck"
+__version__   = "$Id$"
+
+
+def test_import():
+    '''Verify successful import of top-level "tws" package'''
+    import tws
+    assert tws
