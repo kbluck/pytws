6ab8fda76011f0488c242f17277dd8d307feb5f1
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-11-23 05:17:17 +0000

    Added constants to EClientSocket.

--- trunk/tws/_EClientSocket.py
+++ trunk/tws/_EClientSocket.py
@@ -11,4 +11,72 @@ __version__   = "$Id$"
 class EClientSocket(object):
     """Socket client which connects to the TWS socket server.
     """
-    pass
+
+    # General constants
+    CLIENT_VERSION = 42
+    SERVER_VERSION = 38
+    EOL = 0
+    BAG_SEC_TYPE = "BAG"
+
+    # API tag constants
+    REQ_MKT_DATA = 1
+    CANCEL_MKT_DATA = 2
+    PLACE_ORDER = 3
+    CANCEL_ORDER = 4
+    REQ_OPEN_ORDERS = 5
+    REQ_ACCOUNT_DATA = 6
+    REQ_EXECUTIONS = 7
+    REQ_IDS = 8
+    REQ_CONTRACT_DATA = 9
+    REQ_MKT_DEPTH = 10
+    CANCEL_MKT_DEPTH = 11
+    REQ_NEWS_BULLETINS = 12
+    CANCEL_NEWS_BULLETINS = 13
+    SET_SERVER_LOGLEVEL = 14
+    REQ_AUTO_OPEN_ORDERS = 15
+    REQ_ALL_OPEN_ORDERS = 16
+    REQ_MANAGED_ACCTS = 17
+    REQ_FA = 18
+    REPLACE_FA = 19
+    REQ_HISTORICAL_DATA = 20
+    EXERCISE_OPTIONS = 21
+    REQ_SCANNER_SUBSCRIPTION = 22
+    CANCEL_SCANNER_SUBSCRIPTION = 23
+    REQ_SCANNER_PARAMETERS = 24
+    CANCEL_HISTORICAL_DATA = 25
+    REQ_CURRENT_TIME = 49
+    REQ_REAL_TIME_BARS = 50
+    CANCEL_REAL_TIME_BARS = 51
+    REQ_FUNDAMENTAL_DATA = 52
+    CANCEL_FUNDAMENTAL_DATA = 53
+    MIN_SERVER_VER_REAL_TIME_BARS = 34
+    MIN_SERVER_VER_SCALE_ORDERS = 35
+    MIN_SERVER_VER_SNAPSHOT_MKT_DATA = 35
+    MIN_SERVER_VER_SSHORT_COMBO_LEGS = 35
+    MIN_SERVER_VER_WHAT_IF_ORDERS = 36
+    MIN_SERVER_VER_CONTRACT_CONID = 37
+    MIN_SERVER_VER_PTA_ORDERS = 39
+    MIN_SERVER_VER_FUNDAMENTAL_DATA = 40
+    MIN_SERVER_VER_UNDER_COMP = 40
+    MIN_SERVER_VER_CONTRACT_DATA_CHAIN = 40
+    MIN_SERVER_VER_SCALE_ORDERS2 = 40
+    MIN_SERVER_VER_ALGO_ORDERS = 41
+    MIN_SERVER_VER_EXECUTION_DATA_CHAIN = 42
+
+    # Message Type name constants
+    GROUPS = 1
+    PROFILES = 2
+    ALIASES = 3
+
+    @classmethod
+    def faMsgTypeName(cls, faDataType):
+        if faDataType == cls.GROUPS:
+            return "GROUPS"
+        elif faDataType == cls.PROFILES:
+            return "PROFILES"
+        elif faDataType == cls.ALIASES:
+            return "ALIASES"
+
+        # Should never get here.
+        assert False
+        return ""
