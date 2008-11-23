780129db10c7923e1f03e1e0e27f84682252ea99
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-11-23 05:37:20 +0000

    Added simple getter methods.

--- test_tws/test_EClientSocket.py
+++ test_tws/test_EClientSocket.py
@@ -7,16 +7,32 @@ import unittest
 from tws import EClientSocket
 
 
+# Local classes required to test EClientSocket
+class _wrapper(object):
+    pass
+
+class _reader(object):
+    pass
+
+
 class test_EClientSocket(unittest.TestCase):
     '''Test class "tws.EClientSocket"'''
+    
+    def setUp(self):
+        self.wrapper = _wrapper()
+        self.client = EClientSocket(self.wrapper)
 
     def test_api_versions(self):
         # Want to make sure to notice version changes.
         self.assertEqual(EClientSocket.CLIENT_VERSION, 42)
         self.assertEqual(EClientSocket.SERVER_VERSION, 38)
 
-    def test_init(self):
-        self.assertTrue(EClientSocket())
+    def test_getters(self):
+        self.assertEqual(self.client.wrapper(), self.wrapper)
+        self.assertEqual(self.client.reader(), None)
+        self.assertEqual(self.client.isConnected(), False)
+        self.assertEqual(self.client.serverVersion(), "")
+        self.assertEqual(self.client.TwsConnectionTime(), "")
 
     def test_MsgTypeName(self):
         self.assertEqual(EClientSocket.faMsgTypeName(EClientSocket.GROUPS), "GROUPS")
