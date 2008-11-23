0ffce40eae01af27415f2f8abb39f53ff21dfb67
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-11-23 04:23:50 +0000

    Implemented module EClientErrors

---
+++ test_tws/test_EClientErrors.py
@@ -0,0 +1,28 @@
+'''Unit test package for module "tws.EClientErrors".'''
+
+__copyright__ = "Copyright (c) 2008 Kevin J Bluck"
+__version__   = "$Id$"
+
+import unittest
+from tws import EClientErrors
+from tws.EClientErrors import CodeMsgPair
+
+
+class test_EClientErrors(unittest.TestCase):
+    '''Test module "tws.EClientErrors"'''
+
+    def test_CodeMsgPair(self):
+        test1 = CodeMsgPair((1,"Test1"))
+        test2 = CodeMsgPair((2,"Test2"))
+
+        self.assertEqual(test1.code(), 1)
+        self.assertEqual(test2.code(), 2)
+        self.assertEqual(test1.msg(), "Test1")
+        self.assertEqual(test2.msg(), "Test2")
+
+        if __debug__:
+            self.assertRaises(TypeError, CodeMsgPair, 1)
+            self.assertRaises(AssertionError, CodeMsgPair, [])
+            self.assertRaises(AssertionError, CodeMsgPair, (1,2,3))
+            self.assertRaises(AssertionError, CodeMsgPair, ("",""))
+            self.assertRaises(AssertionError, CodeMsgPair, (1,2))
