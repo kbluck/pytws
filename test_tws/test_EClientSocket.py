b9c8b969829f7ab8688cfaa956483e9e1d0d057a
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-11-23 05:17:17 +0000

    Added constants to EClientSocket.

--- test_tws/test_EClientSocket.py
+++ test_tws/test_EClientSocket.py
@@ -10,5 +10,19 @@ from tws import EClientSocket
 class test_EClientSocket(unittest.TestCase):
     '''Test class "tws.EClientSocket"'''
 
+    def test_api_versions(self):
+        # Want to make sure to notice version changes.
+        self.assertEqual(EClientSocket.CLIENT_VERSION, 42)
+        self.assertEqual(EClientSocket.SERVER_VERSION, 38)
+
     def test_init(self):
         self.assertTrue(EClientSocket())
+
+    def test_MsgTypeName(self):
+        self.assertEqual(EClientSocket.faMsgTypeName(EClientSocket.GROUPS), "GROUPS")
+        self.assertEqual(EClientSocket.faMsgTypeName(EClientSocket.PROFILES), "PROFILES")
+        self.assertEqual(EClientSocket.faMsgTypeName(EClientSocket.ALIASES), "ALIASES")
+
+        if __debug__:
+            self.assertRaises(AssertionError, EClientSocket.faMsgTypeName, 0)
+            self.assertRaises(AssertionError, EClientSocket.faMsgTypeName, 4)
