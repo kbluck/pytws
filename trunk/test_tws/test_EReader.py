089d23ae49ca8e3d2422d18f19787bc02d11f0b6
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-11-24 00:41:44 +0000

    Implemented EReader.__init__()

--- trunk/test_tws/test_EReader.py
+++ trunk/test_tws/test_EReader.py
@@ -4,11 +4,23 @@ __copyright__ = "Copyright (c) 2008 Kevin J Bluck"
 __version__   = "$Id$"
 
 import unittest
-from tws import EReader
+from StringIO import StringIO
+from tws import EClientSocket, EReader
+from test_tws import mock_wrapper
 
 
 class test_EReader(unittest.TestCase):
     '''Test class "tws.EReader"'''
     
     def setUp(self):
-        self.reader = EReader()
+        self.wrapper = mock_wrapper()
+        self.parent = EClientSocket(self.wrapper)
+        self.stream = StringIO()
+        self.reader = EReader(self.parent, self.stream)
+
+    def test_init(self):
+        self.assertTrue(EReader(self.parent, self.stream))
+
+        if __debug__:
+            self.assertRaises(AssertionError, EReader, 1, self.stream)
+            self.assertRaises(AssertionError, EReader, self.parent, 1)
