a8e63ba296754ffe6448b3f895987ed103d64780
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-12-01 00:44:39 +0000

    Added standard EReader message tag values.

--- trunk/tws/_EReader.py
+++ trunk/tws/_EReader.py
@@ -62,6 +62,42 @@ class EReader(object):
         return self._readDouble(default=self._DOUBLE_MAX_VALUE)
 
 
+    ## Tag constants ##
+
+    TICK_PRICE = 1
+    TICK_SIZE = 2
+    ORDER_STATUS = 3
+    ERR_MSG = 4
+    OPEN_ORDER = 5
+    ACCT_VALUE = 6
+    PORTFOLIO_VALUE = 7
+    ACCT_UPDATE_TIME = 8
+    NEXT_VALID_ID = 9
+    CONTRACT_DATA = 10
+    EXECUTION_DATA = 11
+    MARKET_DEPTH = 12
+    MARKET_DEPTH_L2 = 13
+    NEWS_BULLETINS = 14
+    MANAGED_ACCTS = 15
+    RECEIVE_FA = 16
+    HISTORICAL_DATA = 17
+    BOND_CONTRACT_DATA = 18
+    SCANNER_PARAMETERS = 19
+    SCANNER_DATA = 20
+    TICK_OPTION_COMPUTATION = 21
+    TICK_GENERIC = 45
+    TICK_STRING = 46
+    TICK_EFP = 47
+    CURRENT_TIME = 49
+    REAL_TIME_BARS = 50
+    FUNDAMENTAL_DATA = 51
+    CONTRACT_DATA_END = 52
+    OPEN_ORDER_END = 53
+    ACCT_DOWNLOAD_END = 54
+    EXECUTION_DATA_END = 55
+    DELTA_NEUTRAL_VALIDATION = 56
+
+
     ## Private class imports ##
 
     from cStringIO import StringIO as _buffer_factory
