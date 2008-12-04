eabf0d84d3291b96ed8a95ece573e6f9c29ed5c5
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-12-04 20:41:41 +0000

    Implemented TagValue data structure.

---
+++ test_tws/test_TagValue.py
@@ -0,0 +1,39 @@
+'''Unit test package for module "tws._TagValue".'''
+
+__copyright__ = "Copyright (c) 2008 Kevin J Bluck"
+__version__   = "$Id$"
+
+import unittest
+from tws import TagValue
+
+
+class test_TagValue(unittest.TestCase):
+    '''Test type "tws.TagValue"'''
+
+    def test_init(self):
+        test1 = TagValue()
+        test2 = TagValue('AB','CD')
+        self.assertEqual(test1.m_tag, '')
+        self.assertEqual(test1.m_value, '')
+        self.assertEqual(test2.m_tag, 'AB')
+        self.assertEqual(test2.m_value, 'CD')
+
+    def test_fields(self):
+        test = TagValue()
+        test.m_tag = 'ab'
+        test.m_value = 'cd'
+        self.assertEqual(test.m_tag, 'ab')
+        self.assertEqual(test.m_value, 'cd')
+
+    def test_equals(self):
+        test1 = TagValue()
+        test2 = TagValue()
+        test3 = TagValue('AB','CD')
+        
+        self.assertEqual(test1, test1)
+        self.assertEqual(test1, test2)
+        self.assertNotEqual(test1, None)
+        self.assertNotEqual(test1, "")
+        self.assertEqual(test3, test3)
+        self.assertNotEqual(test1, test3)
+        
---
+++ tws/_TagValue.py
@@ -0,0 +1,23 @@
+'''TagValue data structure.'''
+
+__copyright__ = "Copyright (c) 2008 Kevin J Bluck"
+__version__   = "$Id$"
+
+
+class TagValue(object):
+    '''Tag/Value pair data structure.
+    
+       Used to pass parameters for Algo orders.
+    '''
+
+    def __init__(self, tag="", value=""):
+        self.m_tag = tag
+        self.m_value = value
+
+
+    def __eq__(self, other):
+        if id(self) == id(other): return True
+        if not isinstance(other, self.__class__): return False
+        if (self.m_tag != other.m_tag): return False
+        if (self.m_value != other.m_value): return False
+        return True
