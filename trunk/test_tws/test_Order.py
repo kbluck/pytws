68eef5f007f56f12127f748dead0569ba5c93473
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-12-08 00:34:28 +0000

    Implemented tws.Order

---
+++ trunk/test_tws/test_Order.py
@@ -0,0 +1,294 @@
+'''Unit test package for module "tws._Order".'''
+
+__copyright__ = "Copyright (c) 2008 Kevin J Bluck"
+__version__   = "$Id$"
+
+import unittest
+from tws import Order
+
+
+class test_Order(unittest.TestCase):
+    '''Test type "tws.Order"'''
+
+    def test_init(self):
+        test1 = Order()
+    
+        self.assertEqual(test1.m_orderId, 0)
+        self.assertEqual(test1.m_clientId, 0)
+        self.assertEqual(test1.m_permId, 0)
+        self.assertEqual(test1.m_action, Order.EMPTY_STR)
+        self.assertEqual(test1.m_totalQuantity, 0)
+        self.assertEqual(test1.m_orderType, Order.EMPTY_STR)
+        self.assertEqual(test1.m_lmtPrice, 0.0)
+        self.assertEqual(test1.m_auxPrice, 0.0)
+        self.assertEqual(test1.m_tif, Order.EMPTY_STR)
+        self.assertEqual(test1.m_ocaGroup, Order.EMPTY_STR)
+        self.assertEqual(test1.m_ocaType, 0)
+        self.assertEqual(test1.m_orderRef, Order.EMPTY_STR)
+        self.assertEqual(test1.m_transmit, True)
+        self.assertEqual(test1.m_parentId, 0)
+        self.assertEqual(test1.m_blockOrder, False)
+        self.assertEqual(test1.m_sweepToFill, False)
+        self.assertEqual(test1.m_displaySize, 0)
+        self.assertEqual(test1.m_triggerMethod, 0)
+        self.assertEqual(test1.m_outsideRth, False)
+        self.assertEqual(test1.m_hidden, False)
+        self.assertEqual(test1.m_goodAfterTime, Order.EMPTY_STR)
+        self.assertEqual(test1.m_goodTillDate, Order.EMPTY_STR)
+        self.assertEqual(test1.m_overridePercentageConstraints, False)
+        self.assertEqual(test1.m_rule80A, Order.EMPTY_STR)
+        self.assertEqual(test1.m_allOrNone, False)
+        self.assertEqual(test1.m_minQty, Order._INT_MAX_VALUE)
+        self.assertEqual(test1.m_percentOffset, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_trailStopPrice, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_faGroup, Order.EMPTY_STR)
+        self.assertEqual(test1.m_faProfile, Order.EMPTY_STR)
+        self.assertEqual(test1.m_faMethod, Order.EMPTY_STR)
+        self.assertEqual(test1.m_faPercentage, Order.EMPTY_STR)
+        self.assertEqual(test1.m_openClose, "O")
+        self.assertEqual(test1.m_origin, Order.CUSTOMER)
+        self.assertEqual(test1.m_shortSaleSlot, 0)
+        self.assertEqual(test1.m_designatedLocation, Order.EMPTY_STR)
+        self.assertEqual(test1.m_discretionaryAmt, 0.0)
+        self.assertEqual(test1.m_eTradeOnly, False)
+        self.assertEqual(test1.m_firmQuoteOnly, False)
+        self.assertEqual(test1.m_nbboPriceCap, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_auctionStrategy, 0)
+        self.assertEqual(test1.m_startingPrice, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_stockRefPrice, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_delta, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_stockRangeLower, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_stockRangeUpper, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_volatility, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_volatilityType, Order._INT_MAX_VALUE)
+        self.assertEqual(test1.m_continuousUpdate, 0)
+        self.assertEqual(test1.m_referencePriceType, Order._INT_MAX_VALUE)
+        self.assertEqual(test1.m_deltaNeutralOrderType, Order.EMPTY_STR)
+        self.assertEqual(test1.m_deltaNeutralAuxPrice, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_basisPoints, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_basisPointsType, Order._INT_MAX_VALUE)
+        self.assertEqual(test1.m_scaleInitLevelSize, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_scaleSubsLevelSize, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_scalePriceIncrement, Order._DOUBLE_MAX_VALUE)
+        self.assertEqual(test1.m_account, Order.EMPTY_STR)
+        self.assertEqual(test1.m_settlingFirm, Order.EMPTY_STR)
+        self.assertEqual(test1.m_clearingAccount, Order.EMPTY_STR)
+        self.assertEqual(test1.m_clearingIntent, Order.EMPTY_STR)
+        self.assertEqual(test1.m_algoStrategy, Order.EMPTY_STR)
+        self.assertEqual(test1.m_algoParams, [])
+        self.assertEqual(test1.m_whatIf, False)
+
+    def test_fields(self):
+        test = Order()
+
+        test.m_orderId = 1
+        test.m_clientId = 2
+        test.m_permId = 3
+        test.m_action = 'a1'
+        test.m_totalQuantity = 4
+        test.m_orderType = 'b2'
+        test.m_lmtPrice = 1.5
+        test.m_auxPrice = 2.5
+        test.m_tif = 'c3'
+        test.m_ocaGroup = 'd4'
+        test.m_ocaType = 5
+        test.m_orderRef = 'e5'
+        test.m_transmit = False
+        test.m_parentId = 6
+        test.m_blockOrder = True
+        test.m_sweepToFill = True
+        test.m_displaySize = 7
+        test.m_triggerMethod = 8
+        test.m_outsideRth = True
+        test.m_hidden = True
+        test.m_goodAfterTime = 'f7'
+        test.m_goodTillDate = 'g8'
+        test.m_overridePercentageConstraints = True
+        test.m_rule80A = 'h9'
+        test.m_allOrNone = True
+        test.m_minQty = 9
+        test.m_percentOffset = 3.5
+        test.m_trailStopPrice = 4.5
+        test.m_faGroup = 'i1'
+        test.m_faProfile = 'j2'
+        test.m_faMethod = 'k3'
+        test.m_faPercentage = 'l4'
+        test.m_openClose = "1"
+        test.m_origin = Order.FIRM
+        test.m_shortSaleSlot = 10
+        test.m_designatedLocation = 'm5'
+        test.m_discretionaryAmt = 5.5
+        test.m_eTradeOnly = True
+        test.m_firmQuoteOnly = True
+        test.m_nbboPriceCap = 6.5
+        test.m_auctionStrategy = 11
+        test.m_startingPrice = 7.5
+        test.m_stockRefPrice = 8.5
+        test.m_delta = 9.5
+        test.m_stockRangeLower = 10.5
+        test.m_stockRangeUpper = 11.5
+        test.m_volatility = 12.5
+        test.m_volatilityType = 12
+        test.m_continuousUpdate = 13
+        test.m_referencePriceType = 14
+        test.m_deltaNeutralOrderType = 'n6'
+        test.m_deltaNeutralAuxPrice = 13.5
+        test.m_basisPoints = 14.5
+        test.m_basisPointsType = 15
+        test.m_scaleInitLevelSize = 15.5
+        test.m_scaleSubsLevelSize = 16.5
+        test.m_scalePriceIncrement = 17.5
+        test.m_account = 'o7'
+        test.m_settlingFirm = 'p8'
+        test.m_clearingAccount = 'q9'
+        test.m_clearingIntent = 'r1'
+        test.m_algoStrategy = 's2'
+        test.m_algoParams = ['t3','u4']
+        test.m_whatIf = True
+
+        self.assertEqual(test.m_orderId, 1)
+        self.assertEqual(test.m_clientId, 2)
+        self.assertEqual(test.m_permId, 3)
+        self.assertEqual(test.m_action, 'a1')
+        self.assertEqual(test.m_totalQuantity, 4)
+        self.assertEqual(test.m_orderType, 'b2')
+        self.assertEqual(test.m_lmtPrice, 1.5)
+        self.assertEqual(test.m_auxPrice, 2.5)
+        self.assertEqual(test.m_tif, 'c3')
+        self.assertEqual(test.m_ocaGroup, 'd4')
+        self.assertEqual(test.m_ocaType, 5)
+        self.assertEqual(test.m_orderRef, 'e5')
+        self.assertEqual(test.m_transmit, False)
+        self.assertEqual(test.m_parentId, 6)
+        self.assertEqual(test.m_blockOrder, True)
+        self.assertEqual(test.m_sweepToFill, True)
+        self.assertEqual(test.m_displaySize, 7)
+        self.assertEqual(test.m_triggerMethod, 8)
+        self.assertEqual(test.m_outsideRth, True)
+        self.assertEqual(test.m_hidden, True)
+        self.assertEqual(test.m_goodAfterTime, 'f7')
+        self.assertEqual(test.m_goodTillDate, 'g8')
+        self.assertEqual(test.m_overridePercentageConstraints, True)
+        self.assertEqual(test.m_rule80A, 'h9')
+        self.assertEqual(test.m_allOrNone, True)
+        self.assertEqual(test.m_minQty, 9)
+        self.assertEqual(test.m_percentOffset, 3.5)
+        self.assertEqual(test.m_trailStopPrice, 4.5)
+        self.assertEqual(test.m_faGroup, 'i1')
+        self.assertEqual(test.m_faProfile, 'j2')
+        self.assertEqual(test.m_faMethod, 'k3')
+        self.assertEqual(test.m_faPercentage, 'l4')
+        self.assertEqual(test.m_openClose, "1")
+        self.assertEqual(test.m_origin, Order.FIRM)
+        self.assertEqual(test.m_shortSaleSlot, 10)
+        self.assertEqual(test.m_designatedLocation, 'm5')
+        self.assertEqual(test.m_discretionaryAmt, 5.5)
+        self.assertEqual(test.m_eTradeOnly, True)
+        self.assertEqual(test.m_firmQuoteOnly, True)
+        self.assertEqual(test.m_nbboPriceCap, 6.5)
+        self.assertEqual(test.m_auctionStrategy, 11)
+        self.assertEqual(test.m_startingPrice, 7.5)
+        self.assertEqual(test.m_stockRefPrice, 8.5)
+        self.assertEqual(test.m_delta, 9.5)
+        self.assertEqual(test.m_stockRangeLower, 10.5)
+        self.assertEqual(test.m_stockRangeUpper, 11.5)
+        self.assertEqual(test.m_volatility, 12.5)
+        self.assertEqual(test.m_volatilityType, 12)
+        self.assertEqual(test.m_continuousUpdate, 13)
+        self.assertEqual(test.m_referencePriceType, 14)
+        self.assertEqual(test.m_deltaNeutralOrderType, 'n6')
+        self.assertEqual(test.m_deltaNeutralAuxPrice, 13.5)
+        self.assertEqual(test.m_basisPoints, 14.5)
+        self.assertEqual(test.m_basisPointsType, 15)
+        self.assertEqual(test.m_scaleInitLevelSize, 15.5)
+        self.assertEqual(test.m_scaleSubsLevelSize, 16.5)
+        self.assertEqual(test.m_scalePriceIncrement, 17.5)
+        self.assertEqual(test.m_account, 'o7')
+        self.assertEqual(test.m_settlingFirm, 'p8')
+        self.assertEqual(test.m_clearingAccount, 'q9')
+        self.assertEqual(test.m_clearingIntent, 'r1')
+        self.assertEqual(test.m_algoStrategy, 's2')
+        self.assertEqual(test.m_algoParams, ['t3','u4'])
+        self.assertEqual(test.m_whatIf, True)
+
+    def test_equals(self):
+        test1 = Order()
+        test2 = Order()
+        test3 = Order()
+        test4 = Order()
+        
+        test3.m_permId = 3
+
+        test4.m_orderId = 1
+        test4.m_clientId = 2
+        test4.m_permId = 3
+        test4.m_action = 'a1'
+        test4.m_totalQuantity = 4
+        test4.m_orderType = 'b2'
+        test4.m_lmtPrice = 1.5
+        test4.m_auxPrice = 2.5
+        test4.m_tif = 'c3'
+        test4.m_ocaGroup = 'd4'
+        test4.m_ocaType = 5
+        test4.m_orderRef = 'e5'
+        test4.m_transmit = False
+        test4.m_parentId = 6
+        test4.m_blockOrder = True
+        test4.m_sweepToFill = True
+        test4.m_displaySize = 7
+        test4.m_triggerMethod = 8
+        test4.m_outsideRth = True
+        test4.m_hidden = True
+        test4.m_goodAfterTime = 'f7'
+        test4.m_goodTillDate = 'g8'
+        test4.m_overridePercentageConstraints = True
+        test4.m_rule80A = 'h9'
+        test4.m_allOrNone = True
+        test4.m_minQty = 9
+        test4.m_percentOffset = 3.5
+        test4.m_trailStopPrice = 4.5
+        test4.m_faGroup = 'i1'
+        test4.m_faProfile = 'j2'
+        test4.m_faMethod = 'k3'
+        test4.m_faPercentage = 'l4'
+        test4.m_openClose = "1"
+        test4.m_origin = Order.FIRM
+        test4.m_shortSaleSlot = 10
+        test4.m_designatedLocation = 'm5'
+        test4.m_discretionaryAmt = 5.5
+        test4.m_eTradeOnly = True
+        test4.m_firmQuoteOnly = True
+        test4.m_nbboPriceCap = 6.5
+        test4.m_auctionStrategy = 11
+        test4.m_startingPrice = 7.5
+        test4.m_stockRefPrice = 8.5
+        test4.m_delta = 9.5
+        test4.m_stockRangeLower = 10.5
+        test4.m_stockRangeUpper = 11.5
+        test4.m_volatility = 12.5
+        test4.m_volatilityType = 12
+        test4.m_continuousUpdate = 13
+        test4.m_referencePriceType = 14
+        test4.m_deltaNeutralOrderType = 'n6'
+        test4.m_deltaNeutralAuxPrice = 13.5
+        test4.m_basisPoints = 14.5
+        test4.m_basisPointsType = 15
+        test4.m_scaleInitLevelSize = 15.5
+        test4.m_scaleSubsLevelSize = 16.5
+        test4.m_scalePriceIncrement = 17.5
+        test4.m_account = 'o7'
+        test4.m_settlingFirm = 'p8'
+        test4.m_clearingAccount = 'q9'
+        test4.m_clearingIntent = 'r1'
+        test4.m_algoStrategy = 's2'
+        test4.m_algoParams = ['t3','u4']
+        test4.m_whatIf = True
+
+        self.assertEqual(test1, test1)
+        self.assertEqual(test1, test2)
+        self.assertNotEqual(test1, None)
+        self.assertNotEqual(test1, "")
+        self.assertEqual(test3, test3)
+        self.assertEqual(test3, test4)
+        self.assertEqual(test1, test3)    # Strange but true. IB bug?
+        self.assertNotEqual(test1, test4)
---
+++ trunk/tws/_Order.py
@@ -0,0 +1,166 @@
+'''Order data structure.'''
+
+__copyright__ = "Copyright (c) 2008 Kevin J Bluck"
+__version__   = "$Id$"
+
+from tws import _Util
+
+
+class Order(object):
+    '''Data structure with attributes used to describe an order.
+    '''
+
+    def __init__(self):
+        self.m_orderId = 0
+        self.m_clientId = 0
+        self.m_permId = 0
+        self.m_action = self.EMPTY_STR
+        self.m_totalQuantity = 0
+        self.m_orderType = self.EMPTY_STR
+        self.m_lmtPrice = 0.0
+        self.m_auxPrice = 0.0
+        self.m_tif = self.EMPTY_STR
+        self.m_ocaGroup = self.EMPTY_STR
+        self.m_ocaType = 0
+        self.m_orderRef = self.EMPTY_STR
+        self.m_transmit = True
+        self.m_parentId = 0
+        self.m_blockOrder = False
+        self.m_sweepToFill = False
+        self.m_displaySize = 0
+        self.m_triggerMethod = 0
+        self.m_outsideRth = False
+        self.m_hidden = False
+        self.m_goodAfterTime = self.EMPTY_STR
+        self.m_goodTillDate = self.EMPTY_STR
+        self.m_overridePercentageConstraints = False
+        self.m_rule80A = self.EMPTY_STR
+        self.m_allOrNone = False
+        self.m_minQty = self._INT_MAX_VALUE
+        self.m_percentOffset = self._DOUBLE_MAX_VALUE
+        self.m_trailStopPrice = self._DOUBLE_MAX_VALUE
+        self.m_faGroup = self.EMPTY_STR
+        self.m_faProfile = self.EMPTY_STR
+        self.m_faMethod = self.EMPTY_STR
+        self.m_faPercentage = self.EMPTY_STR
+        self.m_openClose = "O"
+        self.m_origin = self.CUSTOMER
+        self.m_shortSaleSlot = 0
+        self.m_designatedLocation = self.EMPTY_STR
+        self.m_discretionaryAmt = 0.0
+        self.m_eTradeOnly = False
+        self.m_firmQuoteOnly = False
+        self.m_nbboPriceCap = self._DOUBLE_MAX_VALUE
+        self.m_auctionStrategy = 0
+        self.m_startingPrice = self._DOUBLE_MAX_VALUE
+        self.m_stockRefPrice = self._DOUBLE_MAX_VALUE
+        self.m_delta = self._DOUBLE_MAX_VALUE
+        self.m_stockRangeLower = self._DOUBLE_MAX_VALUE
+        self.m_stockRangeUpper = self._DOUBLE_MAX_VALUE
+        self.m_volatility = self._DOUBLE_MAX_VALUE
+        self.m_volatilityType = self._INT_MAX_VALUE
+        self.m_continuousUpdate = 0
+        self.m_referencePriceType = self._INT_MAX_VALUE
+        self.m_deltaNeutralOrderType = self.EMPTY_STR
+        self.m_deltaNeutralAuxPrice = self._DOUBLE_MAX_VALUE
+        self.m_basisPoints = self._DOUBLE_MAX_VALUE
+        self.m_basisPointsType = self._INT_MAX_VALUE
+        self.m_scaleInitLevelSize = self._DOUBLE_MAX_VALUE
+        self.m_scaleSubsLevelSize = self._DOUBLE_MAX_VALUE
+        self.m_scalePriceIncrement = self._DOUBLE_MAX_VALUE
+        self.m_account = self.EMPTY_STR
+        self.m_settlingFirm = self.EMPTY_STR
+        self.m_clearingAccount = self.EMPTY_STR
+        self.m_clearingIntent = self.EMPTY_STR
+        self.m_algoStrategy = self.EMPTY_STR
+        self.m_algoParams = []
+        self.m_whatIf = False
+
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__): return False
+        if (self.m_permId == other.m_permId): return True
+        return True if (
+            (self.m_orderId == other.m_orderId) and
+            (self.m_clientId == other.m_clientId) and
+            (self.m_totalQuantity == other.m_totalQuantity) and
+            (self.m_lmtPrice == other.m_lmtPrice) and
+            (self.m_auxPrice == other.m_auxPrice) and
+            (self.m_ocaType == other.m_ocaType) and
+            (self.m_transmit == other.m_transmit) and
+            (self.m_parentId == other.m_parentId) and
+            (self.m_blockOrder == other.m_blockOrder) and
+            (self.m_sweepToFill == other.m_sweepToFill) and
+            (self.m_displaySize == other.m_displaySize) and
+            (self.m_triggerMethod == other.m_triggerMethod) and
+            (self.m_outsideRth == other.m_outsideRth) and
+            (self.m_hidden == other.m_hidden) and
+            (self.m_overridePercentageConstraints == other.m_overridePercentageConstraints) and
+            (self.m_allOrNone == other.m_allOrNone) and
+            (self.m_minQty == other.m_minQty) and
+            (self.m_percentOffset == other.m_percentOffset) and
+            (self.m_trailStopPrice == other.m_trailStopPrice) and
+            (self.m_origin == other.m_origin) and
+            (self.m_shortSaleSlot == other.m_shortSaleSlot) and
+            (self.m_discretionaryAmt == other.m_discretionaryAmt) and
+            (self.m_eTradeOnly == other.m_eTradeOnly) and
+            (self.m_firmQuoteOnly == other.m_firmQuoteOnly) and
+            (self.m_nbboPriceCap == other.m_nbboPriceCap) and
+            (self.m_auctionStrategy == other.m_auctionStrategy) and
+            (self.m_startingPrice == other.m_startingPrice) and
+            (self.m_stockRefPrice == other.m_stockRefPrice) and
+            (self.m_delta == other.m_delta) and 
+            (self.m_stockRangeLower == other.m_stockRangeLower) and 
+            (self.m_stockRangeUpper == other.m_stockRangeUpper) and
+            (self.m_volatility == other.m_volatility) and
+            (self.m_volatilityType == other.m_volatilityType) and
+            (self.m_continuousUpdate == other.m_continuousUpdate) and
+            (self.m_referencePriceType == other.m_referencePriceType) and
+            (self.m_deltaNeutralAuxPrice == other.m_deltaNeutralAuxPrice) and
+            (self.m_basisPoints == other.m_basisPoints) and
+            (self.m_basisPointsType == other.m_basisPointsType) and
+            (self.m_scaleInitLevelSize == other.m_scaleInitLevelSize) and
+            (self.m_scaleSubsLevelSize == other.m_scaleSubsLevelSize) and
+            (self.m_scalePriceIncrement == other.m_scalePriceIncrement) and
+            (self.m_whatIf == other.m_whatIf) and
+            (self.m_action == other.m_action) and
+            (self.m_orderType == other.m_orderType) and
+            (self.m_tif == other.m_tif) and
+            (self.m_ocaGroup == other.m_ocaGroup) and
+            (self.m_orderRef == other.m_orderRef) and
+            (self.m_goodAfterTime == other.m_goodAfterTime)and
+            (self.m_goodTillDate == other.m_goodTillDate) and
+            (self.m_rule80A == other.m_rule80A) and
+            (self.m_faGroup == other.m_faGroup) and
+            (self.m_faProfile == other.m_faProfile) and
+            (self.m_faMethod == other.m_faMethod) and
+            (self.m_faPercentage == other.m_faPercentage) and
+            (self.m_openClose == other.m_openClose) and
+            (self.m_designatedLocation == other.m_designatedLocation) and
+            (self.m_deltaNeutralOrderType == other.m_deltaNeutralOrderType) and
+            (self.m_account == other.m_account) and
+            (self.m_settlingFirm == other.m_settlingFirm) and
+            (self.m_clearingAccount == other.m_clearingAccount)and
+            (self.m_clearingIntent == other.m_clearingIntent) and
+            (self.m_algoStrategy == other.m_algoStrategy) and
+             _Util.VectorEqualsUnordered(self.m_algoParams, other.m_algoParams)
+        ) else False
+
+
+    # Class constants
+    CUSTOMER = 0
+    FIRM = 1
+    OPT_UNKNOWN = '?'
+    OPT_BROKER_DEALER = 'b'
+    OPT_CUSTOMER = 'c'
+    OPT_FIRM = 'f'
+    OPT_ISEMM = 'm'
+    OPT_FARMM = 'n'
+    OPT_SPECIALIST = 'y'
+    AUCTION_MATCH = 1
+    AUCTION_IMPROVEMENT = 2
+    AUCTION_TRANSPARENT = 3
+    EMPTY_STR = ""
+
+    from tws._Util import _INT_MAX_VALUE
+    from tws._Util import _DOUBLE_MAX_VALUE
