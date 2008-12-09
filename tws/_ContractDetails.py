053b908f9f455ef1baaff88b1dddbbf533f60e5a
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-12-09 06:21:27 +0000

    Implemented an __eq__() method for tws.ContractDetails

--- tws/_ContractDetails.py
+++ tws/_ContractDetails.py
@@ -37,3 +37,31 @@ class ContractDetails(object):
         self.m_nextOptionType = ""
         self.m_nextOptionPartial = False
         self.m_notes = ""
+
+
+    def __eq__(self, other):  # Not in Java source. IB bug?  
+        if not isinstance(other, self.__class__): return False
+        return True if (
+            (self.m_summary == other.m_summary) and
+            (self.m_marketName == other.m_marketName) and
+            (self.m_tradingClass == other.m_tradingClass) and
+            (self.m_minTick == other.m_minTick) and
+            (self.m_orderTypes == other.m_orderTypes) and
+            (self.m_validExchanges == other.m_validExchanges) and
+            (self.m_underConId == other.m_underConId) and
+            (self.m_cusip == other.m_cusip) and
+            (self.m_ratings == other.m_ratings) and
+            (self.m_descAppend == other.m_descAppend) and
+            (self.m_bondType == other.m_bondType) and
+            (self.m_couponType == other.m_couponType) and
+            (self.m_callable == other.m_callable) and
+            (self.m_putable == other.m_putable) and
+            (self.m_coupon == other.m_coupon) and
+            (self.m_convertible == other.m_convertible) and
+            (self.m_maturity == other.m_maturity) and
+            (self.m_issueDate == other.m_issueDate) and
+            (self.m_nextOptionDate == other.m_nextOptionDate) and
+            (self.m_nextOptionType == other.m_nextOptionType) and
+            (self.m_nextOptionPartial == other.m_nextOptionPartial) and
+            (self.m_notes == other.m_notes)
+        ) else False
