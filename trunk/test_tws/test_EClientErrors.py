8f15e165d1fe98c9fab228905f736622ffb3465f
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2008-12-23 03:49:52 +0000

    Converted TWSError to accept 3 parameters: id, code, msg.

--- trunk/test_tws/test_EClientErrors.py
+++ trunk/test_tws/test_EClientErrors.py
@@ -12,21 +12,22 @@ class test_EClientErrors(unittest.TestCase):
     '''Test module "tws.EClientErrors"'''
 
     def test_CodeMsgPair(self):
-        test1 = CodeMsgPair((1,"Test1"))
-        test2 = CodeMsgPair((2,"Test2"))
+        test1 = CodeMsgPair(id=3, code=1, msg="Test1")
+        test2 = CodeMsgPair(id=4, code=2, msg="Test2")
 
+        self.assertEqual(test1.id(), 3)
+        self.assertEqual(test2.id(), 4)
         self.assertEqual(test1.code(), 1)
         self.assertEqual(test2.code(), 2)
         self.assertEqual(test1.msg(), "Test1")
         self.assertEqual(test2.msg(), "Test2")
-        self.assertEqual(test1.args, ((1, 'Test1'),))
-        self.assertEqual(test2.args, ((2, 'Test2'),))
-        self.assertEqual(test1.message, "1: Test1")
-        self.assertEqual(test2.message, "2: Test2")
+        self.assertEqual(test1.args, ((3, 1, 'Test1'),))
+        self.assertEqual(test2.args, ((4, 2, 'Test2'),))
+        self.assertEqual(test1.message, "1: Test1 (Id: 3)")
+        self.assertEqual(test2.message, "2: Test2 (Id: 4)")
 
         if __debug__:
-            self.assertRaises(AssertionError, CodeMsgPair, 1)
+            self.assertRaises(AssertionError, CodeMsgPair, id=None)
             self.assertRaises(AssertionError, CodeMsgPair, [])
-            self.assertRaises(AssertionError, CodeMsgPair, (1,2,3))
-            self.assertRaises(AssertionError, CodeMsgPair, ("",""))
-            self.assertRaises(AssertionError, CodeMsgPair, (1,2))
+            self.assertRaises(AssertionError, CodeMsgPair, 1,2,3)
+            self.assertRaises(AssertionError, CodeMsgPair, 1, "","")
