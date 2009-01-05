553e2d86f254612425deaf17101d7ee2e66bf9a5
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-05 06:35:31 +0000

    Implemented tws.UnderComp.__nonzero__()

--- tws/_UnderComp.py
+++ tws/_UnderComp.py
@@ -23,3 +23,7 @@ class UnderComp(object):
             (self.m_delta == other.m_delta) and
             (self.m_price == other.m_price)
         ) else False
+
+
+    def __nonzero__(self):
+        return True if (self.m_conId or self.m_delta or self.m_price) else False
