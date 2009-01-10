636b23d1c88fa8a06af53bebf22355610978982b
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-10 23:55:35 +0000

    Made QueueWrapper a subclass of Queue.Queue

--- trunk/test_tws/test_helper/test_wrapper.py
+++ trunk/test_tws/test_helper/test_wrapper.py
@@ -4,6 +4,7 @@ __copyright__ = "Copyright (c) 2009 Kevin J Bluck"
 __version__   = "$Id$"
 
 import unittest
+import Queue
 from tws import EWrapper
 from tws.helper import QueueWrapper, SynchronizedWrapper
 
@@ -12,6 +13,10 @@ class test_QueueWrapper(unittest.TestCase):
 
     def test_init(self):
         self.assertTrue(isinstance(QueueWrapper(), EWrapper))
+        self.assertTrue(isinstance(QueueWrapper(), Queue.Queue))
+        self.assertTrue(QueueWrapper().empty())
+        self.assertFalse(QueueWrapper().full())
+        self.assertRaises(Queue.Empty, QueueWrapper().get_nowait)
 
 
 class test_SynchronizedWrapper(unittest.TestCase):
