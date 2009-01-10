e27e808c65391b08964d1e1daf73dc5b4de1f476
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-10 16:02:51 +0000

    Added 'tws.helper' package

---
+++ trunk/test_tws/test_helper/__init__.py
@@ -0,0 +1,10 @@
+'''Unit test package for package "tws.helper".'''
+
+__copyright__ = "Copyright (c) 2009 Kevin J Bluck"
+__version__   = "$Id$"
+
+
+def test_import():
+    '''Verify successful import of "tws.helper" package'''
+    import tws.helper
+    assert tws.helper
---
+++ trunk/tws/helper/__init__.py
@@ -0,0 +1,4 @@
+'''Helpers useful for implementing applications with PyTWS.'''
+
+__copyright__ = "Copyright (c) 2009 Kevin J Bluck"
+__version__   = "$Id$"
