1fe2c119908de281bcdadf85d376fab6939aaf56
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-11 00:44:57 +0000

    I guess it's not my place to deny use of put() and put_nowait(), but I am going to assert the expected structure of item.

--- trunk/test_tws/test_helper/test_queuewrapper.py
+++ trunk/test_tws/test_helper/test_queuewrapper.py
@@ -22,8 +22,16 @@ class test_QueueWrapper(unittest.TestCase):
         self.assertEqual(QueueWrapper().qsize(), 0)
         self.assertRaises(Queue.Empty, QueueWrapper().get_nowait)
 
-        self.assertRaises(NotImplementedError, QueueWrapper().put)
-        self.assertRaises(NotImplementedError, QueueWrapper().put_nowait)
+    def test_put(self):
+        self.assertTrue(self.wrapper.empty())
+        self.wrapper.put(item=("test1",("A1",2),{"B2":3}))
+        self.assertFalse(self.wrapper.empty())
+        self.assertEqual(self.wrapper.get(), ("test1",("A1",2),{"B2":3}))
+        
+        self.assertTrue(self.wrapper.empty())
+        self.wrapper.put_nowait(item=("test2",("C3",4),{"D4":5}))
+        self.assertFalse(self.wrapper.empty())
+        self.assertEqual(self.wrapper.get(), ("test2",("C3",4),{"D4":5}))
 
     def test_put_wrapper_call(self):
         self.assertTrue(self.wrapper.empty())
