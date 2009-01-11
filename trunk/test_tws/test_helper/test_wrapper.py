29ad95f1324d30037ba6466ddac79ec5ad05a4ba
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-11 00:21:51 +0000

    Added utility method QueueWrapper._put_wrapper_call()

--- trunk/test_tws/test_helper/test_wrapper.py
+++ trunk/test_tws/test_helper/test_wrapper.py
@@ -11,6 +11,9 @@ from tws.helper import QueueWrapper, SynchronizedWrapper
 
 class test_QueueWrapper(unittest.TestCase):
 
+    def setUp(self):
+        self.wrapper = QueueWrapper() 
+
     def test_init(self):
         self.assertTrue(isinstance(QueueWrapper(), EWrapper))
         self.assertTrue(isinstance(QueueWrapper(), Queue.Queue))
@@ -18,9 +21,16 @@ class test_QueueWrapper(unittest.TestCase):
         self.assertFalse(QueueWrapper().full())
         self.assertEqual(QueueWrapper().qsize(), 0)
         self.assertRaises(Queue.Empty, QueueWrapper().get_nowait)
-        
+
         self.assertRaises(NotImplementedError, QueueWrapper().put)
         self.assertRaises(NotImplementedError, QueueWrapper().put_nowait)
+
+    def test_put_wrapper_call(self):
+        self.assertTrue(self.wrapper.empty())
+        self.wrapper._put_wrapper_call("test", "A1", 2, B2=3)         
+        self.assertFalse(self.wrapper.empty())
+        self.assertEqual(self.wrapper.get(), ("test",("A1",2),{"B2":3}))
+        self.assertTrue(self.wrapper.empty())
 
 
 class test_SynchronizedWrapper(unittest.TestCase):
--- trunk/tws/helper/_wrapper.py
+++ trunk/tws/helper/_wrapper.py
@@ -8,9 +8,16 @@ from tws import EWrapper
 
 
 class QueueWrapper(Queue, EWrapper):
-    
+
+    _queue_type = Queue
+
     def __init__(self):
         super(QueueWrapper, self).__init__()
+
+
+    def _put_wrapper_call(self, method_name, *args, **kwds):
+        QueueWrapper._queue_type.put(self, item=(method_name, args, kwds),
+                                     block=False, timeout=None)
 
 
     def put(self, *args, **kwds):
