386d9e0cb1968390bb92bdad2c263fa5ebe85845
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-11 01:12:55 +0000

    Decided to change the queue item to a tuple of method name string and dict of keyword args. It will be much easier for clients to identify the data they're interested in that way.

--- test_tws/test_helper/test_queuewrapper.py
+++ test_tws/test_helper/test_queuewrapper.py
@@ -5,7 +5,7 @@ __version__   = "$Id$"
 
 import unittest
 import Queue
-from tws import EWrapper
+import tws
 from tws.helper import QueueWrapper
 
 
@@ -15,7 +15,7 @@ class test_QueueWrapper(unittest.TestCase):
         self.wrapper = QueueWrapper() 
 
     def test_init(self):
-        self.assertTrue(isinstance(QueueWrapper(), EWrapper))
+        self.assertTrue(isinstance(QueueWrapper(), tws.EWrapper))
         self.assertTrue(isinstance(QueueWrapper(), Queue.Queue))
         self.assertTrue(QueueWrapper().empty())
         self.assertFalse(QueueWrapper().full())
@@ -24,18 +24,27 @@ class test_QueueWrapper(unittest.TestCase):
 
     def test_put(self):
         self.assertTrue(self.wrapper.empty())
-        self.wrapper.put(item=("test1",("A1",2),{"B2":3}))
+        self.wrapper.put(item=("test1",{"A1":1,"B2":3}))
         self.assertFalse(self.wrapper.empty())
-        self.assertEqual(self.wrapper.get(), ("test1",("A1",2),{"B2":3}))
+        self.assertEqual(self.wrapper.get(), ("test1",{"A1":1,"B2":3}))
         
         self.assertTrue(self.wrapper.empty())
-        self.wrapper.put_nowait(item=("test2",("C3",4),{"D4":5}))
+        self.wrapper.put_nowait(item=("test2",{"C3":2,"D4":4}))
         self.assertFalse(self.wrapper.empty())
-        self.assertEqual(self.wrapper.get(), ("test2",("C3",4),{"D4":5}))
+        self.assertEqual(self.wrapper.get(), ("test2",{"C3":2,"D4":4}))
 
     def test_put_wrapper_call(self):
         self.assertTrue(self.wrapper.empty())
-        self.wrapper._put_wrapper_call("test", "A1", 2, B2=3)         
+        self.wrapper._put_wrapper_call("test", A1=1, B2=3)         
         self.assertFalse(self.wrapper.empty())
-        self.assertEqual(self.wrapper.get(), ("test",("A1",2),{"B2":3}))
+        self.assertEqual(self.wrapper.get(), ("test",{"A1":1,"B2":3}))
         self.assertTrue(self.wrapper.empty())
+
+    def test_wrapper_methods(self):
+        self.assertTrue(self.wrapper.empty())
+
+        self.wrapper.error(tws.EClientErrors.CONNECT_FAIL)
+        self.assertEqual(self.wrapper.get(), ("error",{"e":tws.EClientErrors.CONNECT_FAIL}))
+
+        self.wrapper.connectionClosed()
+        self.assertEqual(self.wrapper.get(), ("connectionClosed",{}))
