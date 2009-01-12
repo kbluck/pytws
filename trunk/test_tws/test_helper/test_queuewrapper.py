1a7b3ea433ece45a5e11acfd215e4d051c12b92a
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-12 00:03:39 +0000

    Finished implementing tws.helper.QueueWrapper

--- trunk/test_tws/test_helper/test_queuewrapper.py
+++ trunk/test_tws/test_helper/test_queuewrapper.py
@@ -44,7 +44,154 @@ class test_QueueWrapper(unittest.TestCase):
         self.assertTrue(self.wrapper.empty())
 
         self.wrapper.error(tws.EClientErrors.CONNECT_FAIL)
-        self.assertEqual(self.wrapper.get(), ("error",{"e":tws.EClientErrors.CONNECT_FAIL}))
+        self.assertEqual(self.wrapper.get(), ("error",
+            {"e":tws.EClientErrors.CONNECT_FAIL}))
 
         self.wrapper.connectionClosed()
-        self.assertEqual(self.wrapper.get(), ("connectionClosed",{}))
+        self.assertEqual(self.wrapper.get(), ("connectionClosed",
+            {}))
+
+        self.wrapper.tickPrice(1,2,3.5,4)
+        self.assertEqual(self.wrapper.get(), ("tickPrice",
+            {"tickerId":1,"field":2,"price":3.5,"canAutoExecute":4}))
+
+        self.wrapper.tickSize(1,2,3)
+        self.assertEqual(self.wrapper.get(), ("tickSize",
+            {"tickerId":1,"field":2,"size":3}))
+
+        self.wrapper.tickOptionComputation(1,2,3.5,4.5,5.5,6.5)
+        self.assertEqual(self.wrapper.get(), ("tickOptionComputation",
+            {"tickerId":1,"field":2,"impliedVol":3.5,"delta":4.5,
+             "modelPrice":5.5,"pvDividend":6.5}))
+
+        self.wrapper.tickGeneric(1,2,3.5)
+        self.assertEqual(self.wrapper.get(), ("tickGeneric",
+            {"tickerId":1,"tickType":2,"value":3.5}))
+
+        self.wrapper.tickString(1,2,"A1")
+        self.assertEqual(self.wrapper.get(), ("tickString",
+            {"tickerId":1,"tickType":2,"value":"A1"}))
+
+        self.wrapper.tickEFP(1,2,3.5,"A1",4.5,5,"B2",6.5,7.5)
+        self.assertEqual(self.wrapper.get(), ("tickEFP",
+            {"tickerId":1,"tickType":2,"basisPoints":3.5,
+             "formattedBasisPoints":"A1","impliedFuture":4.5,
+             "holdDays":5,"futureExpiry":"B2","dividendImpact":6.5,
+             "dividendsToExpiry":7.5}))
+
+        self.wrapper.orderStatus(1,"A1",2,3,4.5,5,6,7.5,8,"B2")
+        self.assertEqual(self.wrapper.get(), ("orderStatus",
+            {"orderId":1,"status":"A1","filled":2,"remaining":3,
+             "avgFillPrice":4.5,"permId":5,"parentId":6,"lastFillPrice":7.5,
+             "clientId":8,"whyHeld":"B2"}))
+
+        self.wrapper.openOrder(1,tws.Contract(),tws.Order(),tws.OrderState())
+        self.assertEqual(self.wrapper.get(), ("openOrder",
+            {"orderId":1,"contract":tws.Contract(),"order":tws.Order(),
+             "orderState":tws.OrderState()}))
+
+        self.wrapper.openOrderEnd()
+        self.assertEqual(self.wrapper.get(), ("openOrderEnd",
+            {}))
+
+        self.wrapper.updateAccountValue("A1","B2","C3","D4")
+        self.assertEqual(self.wrapper.get(), ("updateAccountValue",
+            {"key":"A1","value":"B2","currency":"C3","accountName":"D4"}))
+
+        self.wrapper.updatePortfolio(tws.Contract(),2,3.5,4.5,5.5,6.5,7.5,"A1")
+        self.assertEqual(self.wrapper.get(), ("updatePortfolio",
+            {"contract":tws.Contract(),"position":2,"marketPrice":3.5,
+             "marketValue":4.5,"averageCost":5.5,"unrealizedPNL":6.5,
+             "realizedPNL":7.5,"accountName":"A1"}))
+
+        self.wrapper.updateAccountTime("A1")
+        self.assertEqual(self.wrapper.get(), ("updateAccountTime",
+            {"timeStamp":"A1"}))
+
+        self.wrapper.accountDownloadEnd("A1")
+        self.assertEqual(self.wrapper.get(), ("accountDownloadEnd",
+            {"accountName":"A1"}))
+
+        self.wrapper.nextValidId(1)
+        self.assertEqual(self.wrapper.get(), ("nextValidId",
+            {"orderId":1}))
+
+        self.wrapper.contractDetails(1,tws.ContractDetails())
+        self.assertEqual(self.wrapper.get(), ("contractDetails",
+            {"reqId":1,"contractDetails":tws.ContractDetails()}))
+
+        self.wrapper.bondContractDetails(1,tws.ContractDetails())
+        self.assertEqual(self.wrapper.get(), ("bondContractDetails",
+            {"reqId":1,"contractDetails":tws.ContractDetails()}))
+
+        self.wrapper.contractDetailsEnd(1)
+        self.assertEqual(self.wrapper.get(), ("contractDetailsEnd",
+            {"reqId":1}))
+
+        self.wrapper.execDetails(1,tws.Contract(),tws.Execution())
+        self.assertEqual(self.wrapper.get(), ("execDetails",
+            {"reqId":1,"contract":tws.Contract(),
+             "execution":tws.Execution()}))
+
+        self.wrapper.execDetailsEnd(1)
+        self.assertEqual(self.wrapper.get(), ("execDetailsEnd",
+            {"reqId":1}))
+
+        self.wrapper.updateMktDepth(1,2,3,4,5.5,6)
+        self.assertEqual(self.wrapper.get(), ("updateMktDepth",
+            {"tickerId":1,"position":2,"operation":3,"side":4,
+             "price":5.5,"size":6}))
+
+        self.wrapper.updateMktDepthL2(1,2,"A1",3,4,5.5,6)
+        self.assertEqual(self.wrapper.get(), ("updateMktDepthL2",
+            {"tickerId":1,"position":2,"marketMaker":"A1","operation":3,
+             "side":4,"price":5.5,"size":6}))
+
+        self.wrapper.updateNewsBulletin(1,2,"A1","B2")
+        self.assertEqual(self.wrapper.get(), ("updateNewsBulletin",
+            {"msgId":1,"msgType":2,"message":"A1","origExchange":"B2"}))
+
+        self.wrapper.managedAccounts("A1")
+        self.assertEqual(self.wrapper.get(), ("managedAccounts",
+            {"accountsList":"A1"}))
+
+        self.wrapper.receiveFA(1,"A1")
+        self.assertEqual(self.wrapper.get(), ("receiveFA",
+            {"faDataType":1,"xml":"A1"}))
+
+        self.wrapper.historicalData(1,"A1",2.5,3.5,4.5,5.5,6,7,8.5,True)
+        self.assertEqual(self.wrapper.get(), ("historicalData",
+            {"reqId":1,"date":"A1","open":2.5,"high":3.5,"low":4.5,
+             "close":5.5,"volume":6,"count":7,"wap":8.5,"hasGaps":True}))
+
+        self.wrapper.scannerParameters("A1")
+        self.assertEqual(self.wrapper.get(), ("scannerParameters",
+            {"xml":"A1"}))
+
+        self.wrapper.scannerData(1,2,tws.ContractDetails(),"A1","B2",
+                                 "C3","D4")
+        self.assertEqual(self.wrapper.get(), ("scannerData",
+            {"reqId":1,"rank":2,"contractDetails":tws.ContractDetails(),
+             "distance":"A1","benchmark":"B2","projection":"C3",
+             "legsStr":"D4"}))
+
+        self.wrapper.scannerDataEnd(1)
+        self.assertEqual(self.wrapper.get(), ("scannerDataEnd",
+            {"reqId":1}))
+
+        self.wrapper.realtimeBar(1,2,3.5,4.5,5.5,6.5,7,8.5,9)
+        self.assertEqual(self.wrapper.get(), ("realtimeBar",
+            {"reqId":1,"time":2,"open":3.5,"high":4.5,"low":5.5,"close":6.5,
+             "volume":7,"wap":8.5,"count":9}))
+
+        self.wrapper.currentTime(1)
+        self.assertEqual(self.wrapper.get(), ("currentTime",
+            {"time":1}))
+
+        self.wrapper.fundamentalData(1,"A1")
+        self.assertEqual(self.wrapper.get(), ("fundamentalData",
+            {"reqId":1,"data":"A1"}))
+
+        self.wrapper.deltaNeutralValidation(1,tws.UnderComp())
+        self.assertEqual(self.wrapper.get(), ("deltaNeutralValidation",
+            {"reqId":1,"underComp":tws.UnderComp()}))
--- trunk/tws/helper/_queuewrapper.py
+++ trunk/tws/helper/_queuewrapper.py
@@ -37,5 +37,183 @@ class QueueWrapper(Queue, EWrapper):
         self._put_wrapper_call("connectionClosed")
 
 
+    def tickPrice(self, tickerId, field, price, canAutoExecute):
+        self._put_wrapper_call("tickPrice", tickerId=tickerId, field=field,
+                               price=price, canAutoExecute=canAutoExecute)
+
+
+    def tickSize(self, tickerId, field, size):
+        self._put_wrapper_call("tickSize", tickerId=tickerId, field=field,
+                               size=size)
+
+
+    def tickOptionComputation(self, tickerId, field, impliedVol,
+                              delta, modelPrice, pvDividend):
+        self._put_wrapper_call("tickOptionComputation", tickerId=tickerId,
+                               field=field, impliedVol=impliedVol,
+                               delta=delta, modelPrice=modelPrice,
+                               pvDividend=pvDividend)
+
+
+    def tickGeneric(self, tickerId, tickType, value):
+        self._put_wrapper_call("tickGeneric", tickerId=tickerId,
+                               tickType=tickType, value=value)
+
+
+    def tickString(self, tickerId, tickType, value):
+        self._put_wrapper_call("tickString", tickerId=tickerId,
+                               tickType=tickType, value=value)
+
+
+    def tickEFP(self, tickerId, tickType, basisPoints, formattedBasisPoints,
+                impliedFuture, holdDays, futureExpiry, dividendImpact,
+                dividendsToExpiry):
+        self._put_wrapper_call("tickEFP", tickerId=tickerId,
+                               tickType=tickType, basisPoints=basisPoints,
+                               formattedBasisPoints=formattedBasisPoints,
+                               impliedFuture=impliedFuture, holdDays=holdDays,
+                               futureExpiry=futureExpiry,
+                               dividendImpact=dividendImpact,
+                               dividendsToExpiry=dividendsToExpiry)
+
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice,
+                    permId, parentId, lastFillPrice, clientId, whyHeld):
+        self._put_wrapper_call("orderStatus", orderId=orderId, status=status,
+                               filled=filled, remaining=remaining,
+                               avgFillPrice=avgFillPrice, permId=permId,
+                               parentId=parentId, lastFillPrice=lastFillPrice,
+                               clientId=clientId, whyHeld=whyHeld)
+
+
+    def openOrder(self, orderId, contract, order, orderState):
+        self._put_wrapper_call("openOrder", orderId=orderId,
+                               contract=contract, order=order,
+                               orderState=orderState)
+
+
+    def openOrderEnd(self):
+        self._put_wrapper_call("openOrderEnd")
+
+
+    def updateAccountValue(self, key, value, currency, accountName):
+        self._put_wrapper_call("updateAccountValue", key=key, value=value,
+                               currency=currency, accountName=accountName)
+
+
+    def updatePortfolio(self, contract, position, marketPrice, marketValue,
+                        averageCost, unrealizedPNL, realizedPNL, accountName):
+        self._put_wrapper_call("updatePortfolio", contract=contract,
+                               position=position, marketPrice=marketPrice,
+                               marketValue=marketValue,
+                               averageCost=averageCost,
+                               unrealizedPNL=unrealizedPNL,
+                               realizedPNL=realizedPNL,
+                               accountName=accountName)
+
+
+    def updateAccountTime(self, timeStamp):
+        self._put_wrapper_call("updateAccountTime", timeStamp=timeStamp)
+
+
+    def accountDownloadEnd(self, accountName):
+        self._put_wrapper_call("accountDownloadEnd", accountName=accountName)
+
+
+    def nextValidId(self, orderId):
+        self._put_wrapper_call("nextValidId", orderId=orderId)
+
+
+    def contractDetails(self, reqId, contractDetails):
+        self._put_wrapper_call("contractDetails", reqId=reqId,
+                               contractDetails=contractDetails)
+
+
+    def bondContractDetails(self, reqId, contractDetails):
+        self._put_wrapper_call("bondContractDetails", reqId=reqId,
+                               contractDetails=contractDetails)
+
+
+    def contractDetailsEnd(self, reqId):
+        self._put_wrapper_call("contractDetailsEnd", reqId=reqId)
+
+
+    def execDetails(self, reqId, contract, execution):
+        self._put_wrapper_call("execDetails", reqId=reqId, contract=contract,
+                               execution=execution)
+
+
+    def execDetailsEnd(self, reqId):
+        self._put_wrapper_call("execDetailsEnd", reqId=reqId)
+
+
+    def updateMktDepth(self, tickerId, position, operation, side, price, size):
+        self._put_wrapper_call("updateMktDepth", tickerId=tickerId,
+                               position=position, operation=operation,
+                               side=side, price=price, size=size)
+
+
+    def updateMktDepthL2(self, tickerId, position, marketMaker, operation, side, price, size):
+        self._put_wrapper_call("updateMktDepthL2", tickerId=tickerId,
+                               position=position, marketMaker=marketMaker,
+                               operation=operation, side=side, price=price,
+                               size=size)
+
+
+    def updateNewsBulletin(self, msgId, msgType, message, origExchange):
+        self._put_wrapper_call("updateNewsBulletin", msgId=msgId,
+                               msgType=msgType, message=message,
+                               origExchange=origExchange)
+
+
+    def managedAccounts(self, accountsList):
+        self._put_wrapper_call("managedAccounts", accountsList=accountsList)
+
+
+    def receiveFA(self, faDataType, xml):
+        self._put_wrapper_call("receiveFA", faDataType=faDataType, xml=xml)
+
+
+    def historicalData(self, reqId, date, open, high, low, close, volume, count, wap, hasGaps):
+        self._put_wrapper_call("historicalData", reqId=reqId, date=date,
+                               open=open, high=high, low=low, close=close,
+                               volume=volume, count=count, wap=wap,
+                               hasGaps=hasGaps)
+
+
+    def scannerParameters(self, xml):
+        self._put_wrapper_call("scannerParameters", xml=xml)
+
+
+    def scannerData(self, reqId, rank, contractDetails, distance, benchmark, projection, legsStr):
+        self._put_wrapper_call("scannerData", reqId=reqId, rank=rank,
+                               contractDetails=contractDetails,
+                               distance=distance, benchmark=benchmark,
+                               projection=projection, legsStr=legsStr)
+
+
+    def scannerDataEnd(self, reqId):
+        self._put_wrapper_call("scannerDataEnd", reqId=reqId)
+
+
+    def realtimeBar(self, reqId, time, open, high, low, close, volume, wap, count):
+        self._put_wrapper_call("realtimeBar", reqId=reqId, time=time,
+                               open=open, high=high, low=low, close=close,
+                               volume=volume, wap=wap, count=count)
+
+
+    def currentTime(self, time):
+        self._put_wrapper_call("currentTime", time=time)
+
+
+    def fundamentalData(self, reqId, data):
+        self._put_wrapper_call("fundamentalData", reqId=reqId, data=data)
+
+
+    def deltaNeutralValidation(self, reqId, underComp):
+        self._put_wrapper_call("deltaNeutralValidation", reqId=reqId,
+                               underComp=underComp)
+
+
 del EWrapper
 del Queue
