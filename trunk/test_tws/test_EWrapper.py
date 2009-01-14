9ddbb1128855a898d81eb9928cf9af36720beb25
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-14 00:26:45 +0000

    Added a 'logger' property to base EWrapper

--- trunk/test_tws/test_EWrapper.py
+++ trunk/test_tws/test_EWrapper.py
@@ -4,16 +4,28 @@ __copyright__ = "Copyright (c) 2008 Kevin J Bluck"
 __version__   = "$Id$"
 
 import unittest
+import logging
 from tws import EWrapper
-
-
-# Local classes required to test EWrapper
-class _wrapper(EWrapper):
-    pass
 
 
 class test_EWrapper(unittest.TestCase):
     '''Test class "tws.EWrapper"'''
 
     def setUp(self):
-        self.wrapper = _wrapper()
+        self.wrapper = EWrapper()
+
+    def test_init(self):
+        self.assertTrue(EWrapper())
+        self.assertTrue(EWrapper(logging.getLogger("test")))
+
+    def test_logger(self):
+        default_logger = self.wrapper.logger
+        test_logger = logging.getLogger("test")
+        self.assertNotEqual(self.wrapper.logger, test_logger)
+        self.wrapper.logger = test_logger
+        self.assertNotEqual(self.wrapper.logger, default_logger)
+        self.assertEqual(self.wrapper.logger, test_logger)
+
+        if __debug__:
+            try: logging.logger = 123
+            except AssertionError, e: self.assertTrue(e)
