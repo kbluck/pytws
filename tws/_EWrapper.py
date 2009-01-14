305e04798cc86fa36b4274c2c4725ce0610765c2
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-14 00:26:45 +0000

    Added a 'logger' property to base EWrapper

--- tws/_EWrapper.py
+++ tws/_EWrapper.py
@@ -14,4 +14,18 @@ class EWrapper(object):
        A subclass object of this type must be provided to the constructor of
        EClientSocket.
     '''
-    pass
+
+    def __init__(self, logger=None):
+        if logger is None:
+            logger = __import__("logging").getLogger(self.__class__.__name__)
+        self.logger = logger
+
+
+    def logger(self):
+        return self._logger
+    def set_logger(self, new_logger):
+        assert isinstance(new_logger, __import__("logging").Logger)
+        self._logger = new_logger
+    logger = property(fget=logger, fset=set_logger,
+                      doc="logging.Logger object.")
+    del set_logger
