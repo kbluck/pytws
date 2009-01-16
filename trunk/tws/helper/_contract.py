5c89ffbbf7fa91e5926c951f4856ea0deb14025c
kevin.bluck <kevin.bluck@c522fd7c-de71-11dd-bd7b-b12b6590754f>
2009-01-16 19:50:45 +0000

    Docstring edits.

--- trunk/tws/helper/_contract.py
+++ trunk/tws/helper/_contract.py
@@ -7,20 +7,18 @@ from tws import Contract
 
 
 class StockContract(Contract):
-    '''Description of a stock contract.
-
-       Specialized constructor to make it easier to create contract
-       objects referring to a stock instrument.
-       
-       Params:
-         symbol: Always required
-         exchange: optional for US stocks, defaults to "Smart"
-         currency: optional for US stocks.
-         **kwds: Any other keyword args acceptable to tws.Contract
-         may be provided as desired.
-    '''
+    '''Description of a stock contract.'''
 
     def __init__(self, symbol, exchange="SMART", currency="USD", **kwds):
+        '''Specialized constructor for stock contracts.
+
+           Params:
+             symbol: Always required
+             exchange: optional for US stocks, defaults to "Smart"
+             currency: optional for US stocks.
+             **kwds: Any other keyword args acceptable to tws.Contract
+             may be provided as desired.
+        '''
         super(StockContract, self).__init__(sec_type="STK",
                                             symbol=symbol,
                                             exchange=exchange,
@@ -30,23 +28,21 @@ class StockContract(Contract):
 
 
 class FuturesContract(Contract):
-    '''Description of a futures contract.
-
-       Specialized constructor to make it easier to create contract
-       objects referring to a futures instrument.
-       
-       Params:
-         exchange: Always required.
-         symbol: Required if not specifying local_symbol.
-         expiry: Required if not specifying local_symbol.
-         currency: optional for US exchanges.
-         local_symbol: May be specified instead of symbol and expiry.
-         **kwds: Any other keyword args acceptable to tws.Contract
-         may be provided as desired.
-    '''
+    '''Description of a futures contract.'''
 
     def __init__(self, exchange, symbol="", expiry="", currency="USD", 
                  local_symbol="", **kwds):
+        '''Specialized constructor for futures contracts.
+
+           Params:
+             exchange: Always required.
+             symbol: Required if not specifying local_symbol.
+             expiry: Required if not specifying local_symbol.
+             currency: optional for US exchanges.
+             local_symbol: May be specified instead of symbol and expiry.
+             **kwds: Any other keyword args acceptable to tws.Contract
+             may be provided as desired.
+        '''
         assert (symbol and expiry) or local_symbol
 
         super(FuturesContract, self).__init__(sec_type="FUT",
